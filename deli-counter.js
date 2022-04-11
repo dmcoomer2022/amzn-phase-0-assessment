efe155ac438cbf0230362343283f441c4ccd5386
David Coomer <dmcoomer@hotmail.com>
2022-04-10 23:59:47 -0400

    completeed assignmetn

--- deli-counter.js
+++ deli-counter.js
@@ -22,3 +22,35 @@
 // nowServing(katzDeli) //=> "Currently serving Grace."
 
 // line(katzDeli) //=> "The line is currently: 1. Kent 2. Matz"
+
+const katzDeli = ['Ada', 'Grace', 'Kent'];
+
+function line(customersArray) {
+    var counter = 1;
+    var current = '';
+
+    if (!customersArray[0]) {
+        console.log('The line is currently empty');
+    } else {
+        for (let i = 0; i < customersArray.length; i++) {
+            current += `${counter}. ${customersArray[i]} `;
+            counter ++;
+        }
+        return `The line is currently: ` + current;
+    }
+}
+
+function takeANumber(arr, name) {
+    arr.push(name);
+    var namePos = arr.indexOf(name);
+    console.log(`Welcome, ${name}. You are number ${namePos + 1}`);
+}
+
+function nowServing(arr) {
+    if (!arr[0]) {
+        console.log('There is nobody waiting to be served!');
+    } else {
+        console.log(`Currently serving ${arr[0]}.`);
+    arr.shift();  
+    }
+} 
